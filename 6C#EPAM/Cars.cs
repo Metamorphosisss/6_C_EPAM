4793333e78b6c36d10c412f765136f9838b0fc7b
Ivan <mr.komar991@gmail.com>
2024-04-18 21:31:23 +0300

    constructors

---
+++ 6C#EPAM/Cars.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6C_EPAM
+{
+    internal class Cars
+    {
+        public string name;
+        public int max_velocity;
+        public string Name { get; set; }
+        public int Max_Velocity
+        {
+            get { return max_velocity; }
+            set { max_velocity = value; }
+        }
+        public Cars(string carName, int carVelocity)
+        {
+            Name = carName;
+            Max_Velocity = carVelocity;
+        }
+    }
+}
---
+++ 6C#EPAM/Program.cs
@@ -0,0 +1,20 @@
+﻿
+using _6C_EPAM;
+
+Cars Mazda = new Cars("Mazda Miata", 250);
+Cars MercedesBenz = new Cars("Mercedes-Benz Vito", 400);
+
+if (Mazda.Max_Velocity > MercedesBenz.Max_Velocity)
+{
+    Console.WriteLine($"{Mazda.Name} швидша за {MercedesBenz.Name}");
+}
+
+else if (MercedesBenz.Max_Velocity > Mazda.Max_Velocity)
+{
+    Console.WriteLine($"{MercedesBenz.Name} швидша за {Mazda.Name}");
+}
+
+else
+{
+    Console.WriteLine("Визначити найшвидшу машину неможливо");
+}
